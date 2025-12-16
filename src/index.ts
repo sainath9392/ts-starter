6f2415dcbe0d130beab045158c868ffc4f296357
Sainath9392 <sainathduvvuri03@gmail.com>
2025-12-16 16:54:03 +0530

    adding updates

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,12 @@ function greet(name: string) {
 }
 
 console.log(greet("TypeScript"));
+
+// Add this line
+const scores: number[] = [10, 20];
+const firstScore = scores[0];
+const impossibleScore = scores[100];
+
+// Hover over 'impossibleScore'.
+// Because of "noUncheckedIndexedAccess": true,
+// TypeScript will warn you that this might be 'undefined'.
